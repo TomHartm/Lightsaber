bd6ef4938d923287a1f5d1692162bd1f8f74388f
Tom Hartmann <tom.hartmann@tu-bs.de>
2020-10-02 14:24:03 +0200

    Startversion

---
+++ Lightsaber1.1.ino
@@ -0,0 +1,133 @@
+#include "FastLED.h"
+
+
+#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
+#warning "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+#define LED_PIN 11
+
+
+#define LED_TYPE    WS2812
+#define COLOR_ORDER GRB
+#define NUM_LEDS    100
+
+#define switchPin 5
+
+CRGB leds[NUM_LEDS];
+
+#define BRIGHTNESS          100   //alt: 250
+#define FRAMES_PER_SECOND  120
+
+
+const int buttonPin = 2;
+
+int buttonState = LOW;
+int mode = 0;
+int buttonpressed = 0;
+int entprellzeit = 200;
+unsigned long buttontime = 0;
+
+int green = 250;
+int red = 0;
+int blue = 0;
+ 
+bool light_on = false;
+
+void saber_switch();
+
+
+
+
+
+void setup() {
+  pinMode (switchPin, INPUT);
+
+  delay(1000); // 3 second delay for recovery
+
+  Serial.begin(9600);
+  
+  // tell FastLED about the LED strip configuration
+  FastLED.addLeds<LED_TYPE,LED_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  
+  // set master brightness control
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5,3000); 
+
+for (int i = NUM_LEDS-1; i>=0; i--)
+      {
+        leds[i].setRGB(0,0,0);
+        }
+        FastLED.show();
+      
+  delay(1000);
+  saber_switch();
+}
+
+uint8_t gHue = 150; // rotating "base color" used by many of the patterns
+  
+void loop()
+{
+  buttonState = digitalRead(buttonPin);
+  Serial.print(mode);
+
+  if(buttonState == HIGH)
+    {
+      buttontime = millis();
+      buttonpressed = 1;    
+     }
+   if((millis() - buttontime > entprellzeit) && buttonpressed == 1)
+    {
+      mode++;
+      buttonpressed = 0;
+      if(mode == 3) {mode = 0;}
+     }
+    
+   if((millis() - buttontime > entprellzeit) && buttonpressed == 1)
+    {
+      saber_switch(); 
+      buttonpressed = 0;
+    }
+
+
+    if(mode == 0){green = 250;red = 0; blue = 0;}
+    else if(mode == 1){green = 0;red = 0; blue = 250;}
+    else if(mode == 2){green = 0;red = 250; blue = 0;}
+    if(light_on){change_colour();}
+}
+void change_colour(){
+  for (int i = 0; i<NUM_LEDS; i++)
+      {
+        leds[i].setRGB(red,green,blue);
+        
+      }
+    FastLED.show();}
+
+void saber_switch(){
+  if(!light_on){
+  
+  for (int i = 0; i<NUM_LEDS; i++)
+      {
+        leds[i].setRGB(red,green,blue);
+        delay(15);
+        FastLED.show();
+      }
+      light_on = true;
+        
+    }
+
+  else{
+  
+  for (int i = NUM_LEDS-1; i>=0; i--)
+      {
+        leds[i].setRGB(0,0,0);
+        delay(15);
+        FastLED.show();
+        red=0; green=0; blue=0;
+      }
+
+    
+    light_on = false;
+  }
+}
+  
