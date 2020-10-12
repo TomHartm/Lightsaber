65b74d99122f81570a362566057f8b302c9f712f
Tom Hartmann <tom.hartmann@tu-bs.de>
2020-10-12 23:02:05 +0200

    aktueller stand vor test

--- Lightsaber1.1/Lightsaber1.1.ino
+++ Lightsaber1.1/Lightsaber1.1.ino
@@ -4,50 +4,51 @@
 
 /////////////////////////////////////////INITIALISIERUNG/////////////////////////////////////////////////////////
 
-
-
 #define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
 #define NUM_LEDS    100
-#define switchPin 5
+//#define switchPin 5
 #define LED_PIN 11
 
-CRGB leds[NUM_LEDS];
+#define FRAMES_PER_SECOND  60
 
-#define BRIGHTNESS          200   //alt: 250
-#define FRAMES_PER_SECOND  120
+#define buttonPin 2
+#define buttonPin2 3
 
-
-const int buttonPin = 2;
-
-int mode = 0;
-int colour = 0;
+byte mode = 0;
+byte colour = 0;
+bool light_on = false;
+byte BRIGHTNESS = 200;   //alt: 250
 
 //Startfabe
-int green = 250;
-int red = 0;
-int blue = 0;
- 
-bool light_on = false;
+byte green = 250;
+byte red = 0;
+byte blue = 0;
 
+bool men = false;
+CRGB leds[NUM_LEDS];
 
-  OneButton button(buttonPin, false);
+OneButton button(buttonPin, false);
+OneButton button2(buttonPin2, false);
 
-  WS2812FX ws2812fx = WS2812FX(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+WS2812FX ws2812fx = WS2812FX(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 /////////////////////////////////////////SETUP/////////////////////////////////////////////////////////
 
 
 void setup() {
-  pinMode (switchPin, INPUT);
+  pinMode (buttonPin, INPUT);
 
-  delay(1000); // 3 second delay for recovery
+  delay(1000); // 1 second delay for recovery
 
-  Serial.begin(9600);
+ // Serial.begin(9600);
 
   button.attachDoubleClick(change_mode);
   button.attachClick(saber_switch);
   button.attachLongPressStart(change_colour);
+
+  button2.attachLongPressStart(menue);
+  button2.attachClick(brightness_up);
 
   
   // tell FastLED about the LED strip configuration
@@ -58,7 +59,7 @@ void setup() {
   FastLED.setMaxPowerInVoltsAndMilliamps(5,4000); 
 
   ws2812fx.init();
-  ws2812fx.setBrightness(150);
+  ws2812fx.setBrightness(BRIGHTNESS);
   ws2812fx.setSpeed(300);
 
 
@@ -77,13 +78,47 @@ for (int i = NUM_LEDS-1; i>=0; i--)
 void loop()
 {
   button.tick();
+  button2.tick();
   delay(10);
   if(mode != 0 && light_on){
   ws2812fx.service();}
-  Serial.println(ws2812fx.getSpeed());
+//  Serial.println(BRIGHTNESS);
 }
 
 /////////////////////////////////////////FUNKTIONEN/////////////////////////////////////////////////////////
+void menue(){ men = !men;
+  if(men){
+    button.attachClick(brightness_up);
+    button2.attachClick(brightness_down);
+    for (int i = 0; i<100;i++){leds[i] = CRGB::Black;}
+    for(int i = 0; i<BRIGHTNESS/2.5; i++){leds[i].setRGB(red,green,blue);}
+        //FastLED.show();
+    }
+  else{
+    button.attachClick(saber_switch);
+    change_mode();}
+    
+
+}
+
+void brightness_up(){
+    if(BRIGHTNESS<240){BRIGHTNESS +=10;}
+    FastLED.setBrightness(BRIGHTNESS);
+    ws2812fx.setBrightness(BRIGHTNESS);
+    ws2812fx.service();
+        for (int i = 0; i<100;i++){leds[i] = CRGB::Black;}
+    for(int i = 0; i<BRIGHTNESS/2.5; i++){leds[i].setRGB(red,green,blue);}
+    FastLED.show();}
+
+void brightness_down(){
+    if(BRIGHTNESS>30){BRIGHTNESS -=10;}
+    FastLED.setBrightness(BRIGHTNESS);
+    ws2812fx.setBrightness(BRIGHTNESS);
+    ws2812fx.service();
+        for (int i = 0; i<100;i++){leds[i] = CRGB::Black;}
+    for(int i = 0; i<BRIGHTNESS/2.5; i++){leds[i].setRGB(red,green,blue);}
+    FastLED.show();
+    }
 
 void saber_switch(){
   if(!light_on){
@@ -104,13 +139,12 @@ void saber_switch(){
           delay(7);
           FastLED.show();
         }
-
   }
 }
 void change_mode(){
   mode++;
   if(mode == 5){mode = 0;}
-  Serial.println(mode);
+  //Serial.println(mode);
   if(mode == 0){static_mode();}
   else if(mode == 1){smooth();}
   else if(mode == 2){change_color();}
@@ -129,11 +163,10 @@ void change_colour(){
   else if(colour == 5){green = 250;red = 0; blue = 250;}
   else if(colour == 6){green = 150;red = 250; blue = 0;}
 
-  if(light_on){
+  if(light_on && mode ==0){
     for (int i = 0; i<NUM_LEDS; i++)
       {
-        leds[i].setRGB(red,green,blue);
-        
+        leds[i].setRGB(red,green,blue);       
       }
     FastLED.show();}
 }
@@ -145,8 +178,7 @@ void refresh(){
         FastLED.show();
       }}
 
-void static_mode(){refresh();
-}
+void static_mode(){refresh();}
 
 void smooth(){
   ws2812fx.setSpeed(300);
